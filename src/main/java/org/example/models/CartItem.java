2bb58393ce6d0aed42c8adc65f8f6c45e17229a5
init0x1 <abdelrahman.elshaikh7@gmail.com>
2025-07-04 22:06:04 +0300

    Add CartItem Class

---
+++ src/main/java/org/example/models/CartItem.java
@@ -0,0 +1,21 @@
+package org.example.models;
+
+import org.example.models.products.Product;
+
+public class CartItem {
+    private Product product;
+    private int quantity;
+
+    public CartItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct(){
+        return product;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+}
