cc56d459861d120eaa2785a8543df3000f05b4c5
init0x1 <abdelrahman.elshaikh7@gmail.com>
2025-07-04 21:20:57 +0300

    Add PhysicalProduct Class

---
+++ src/main/java/org/example/models/products/PhysicalProduct.java
@@ -0,0 +1,26 @@
+package org.example.models.products;
+
+import org.example.interfaces.Shippable;
+
+public class PhysicalProduct extends Product implements Shippable {
+    private double weight;
+
+    public PhysicalProduct(String name, double price, int quantity, double weight) {
+        super(name, price, quantity);
+        this.weight = weight;
+    }
+
+    @Override
+    public boolean isExpired(){
+        return false;
+    }
+
+    @Override
+    public boolean requiresShipping(){
+        return true;
+    }
+    @Override
+    public double getWeight(){
+        return weight;
+    }
+}
