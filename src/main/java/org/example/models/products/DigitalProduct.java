e35064b278b74885c873112419d6576dcfa292fd
init0x1 <abdelrahman.elshaikh7@gmail.com>
2025-07-04 21:28:34 +0300

    Add DigitalProduct Class

---
+++ src/main/java/org/example/models/products/DigitalProduct.java
@@ -0,0 +1,17 @@
+package org.example.models.products;
+
+public class DigitalProduct extends Product{
+    public DigitalProduct(String name, double price, int quantity) {
+        super(name, price, quantity);
+    }
+
+    @Override
+    public boolean isExpired(){
+        return false;
+    }
+
+    @Override
+    public boolean requiresShipping(){
+        return false;
+    }
+}
